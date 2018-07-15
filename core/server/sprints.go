c129c4ebe404608cf1f41ec5b7a2b9f99d672e95
Karol Ossowski <karol.ossowski@gmail.com>
2018-07-15 14:35:16 +0200

    proto upd + connection updater

--- core/server/sprints.go
+++ core/server/sprints.go
@@ -11,6 +11,7 @@ type Sprints struct {
 	visClient   *VisClient
 	tournament  *pb.Tournament
 	curRace     *pb.Race
+	results     map[int32][]*pb.Result
 }
 
 func SetupSprints(device *device.InputDevice, visClient *VisClient) *Sprints {
@@ -27,23 +28,37 @@ func (s *Sprints) NewTournament(ctx context.Context, tournament *pb.Tournament) 
 
 func (s *Sprints) NewRace(ctx context.Context, race *pb.Race) (*pb.Empty, error) {
 	s.curRace = race
-	return &pb.Empty{}, nil
+	err := s.visClient.NewRace(race)
+	return &pb.Empty{}, err
 }
 
-func (s *Sprints) StartRace(context.Context, *pb.Empty) (*pb.Empty, error) {
-	s.visClient.StartRace()
-
-	return &pb.Empty{}, nil
-}
-
-func (s *Sprints) AbortRace(context.Context, *pb.Empty) (*pb.Empty, error) {
-	s.visClient.AbortRace()
+func (s *Sprints) StartRace(_ context.Context, starter *pb.Starter) (*pb.Empty, error) {
+	s.visClient.StartRace(starter)
 
 	return &pb.Empty{}, nil
 }
 
-func (s *Sprints) ConfigureVis(context.Context, visCfg *pb.VisConfiguration) (*pb.Empty, error) {
+func (s *Sprints) AbortRace(_ context.Context, abortMessage *pb.AbortMessage) (*pb.Empty, error) {
+	s.visClient.AbortRace(abortMessage)
+
+	return &pb.Empty{}, nil
+}
+
+func (s *Sprints) ConfigureVis(_ context.Context, visCfg *pb.VisConfiguration) (*pb.Empty, error) {
 	s.visClient.ConfigureVis(visCfg)
 
 	return &pb.Empty{}, nil
+}
+
+func (s *Sprints) GetResults(resultSpec *pb.ResultSpec, stream pb.Sprints_GetResultsServer) error {
+	for _, result := range s.results[int32(resultSpec.Gender)] {
+		if err := stream.Send(result); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *Sprints) GetTournaments(*pb.Empty, pb.Sprints_GetTournamentsServer) error {
+	panic("not implemented")
 }
--- core/server/vis_client.go
+++ core/server/vis_client.go
@@ -7,6 +7,7 @@ import (
 	pb "github.com/kkoralsky/gosprints/proto"
 	"google.golang.org/grpc"
 	"strings"
+	"time"
 )
 
 type VisClient struct {
@@ -30,16 +31,16 @@ func (v *VisClient) NewRace(race *pb.Race) error {
 	return nil
 }
 
-func (v *VisClient) StartRace() error {
+func (v *VisClient) StartRace(starter *pb.Starter) error {
 	for _, cl := range v.clients {
-		go cl.StartRace(context.Background(), &pb.Empty{})
+		go cl.StartRace(context.Background(), starter)
 	}
 	return nil
 }
 
-func (v *VisClient) AbortRace() error {
+func (v *VisClient) AbortRace(abortMessage *pb.AbortMessage) error {
 	for _, cl := range v.clients {
-		go cl.AbortRace(context.Background(), &pb.Empty{})
+		go cl.AbortRace(context.Background(), abortMessage)
 	}
 	return nil
 }
@@ -51,21 +52,38 @@ func (v *VisClient) ConfigureVis(visCfg *pb.VisConfiguration) error {
 	return nil
 }
 
+func (v *VisClient) connectionStateUpdater() {
+	for i, conn := range v.connections {
+		go func() {
+			for conn != nil {
+				conn.WaitForStateChange(context.Background(), conn.GetState())
+				if conn != nil {
+					core.InfoLogger.Printf("vis connection: %s state: %s",
+						v.addresses[i], conn.GetState().String())
+				}
+			}
+
+			core.InfoLogger.Printf("vis connectino: %s closed", v.addresses[i])
+		}()
+	}
+}
+
 func SetupVisClient(outputs string) (*VisClient, error) {
 	var v = VisClient{
 		addresses: strings.Split(outputs, ","),
 	}
 	for _, addr := range v.addresses {
-		conn, err := grpc.Dial(addr, grpc.WithInsecure())
+		conn, err := grpc.Dial(addr, grpc.WithInsecure(), grpc.WithTimeout(10*time.Second))
 		if err != nil {
 			core.ErrorLogger.Printf("error while dialing to %s: %s\n", addr, err.Error())
 		} else {
 			v.connections = append(v.connections, conn)
 			v.clients = append(v.clients, pb.NewSprintsClient(conn))
-			core.InfoLogger.Printf("dialed to vis: %s\n", addr)
+			core.InfoLogger.Printf("dialed to vis: %s with state: %s\n", addr, conn.GetState().String())
 		}
 	}
 	if len(v.connections) > 0 {
+		go v.connectionStateUpdater()
 		return &v, nil
 	}
 	return &v, fmt.Errorf("couldnt connect to none of the outputs: %s", outputs)
