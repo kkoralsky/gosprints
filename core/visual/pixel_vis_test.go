402c492521550eeb3590e0dfc6cf2d87bc85e143
Karol Ossowski <karol.ossowski@gmail.com>
2018-07-15 14:30:32 +0200

    visual tests

---
+++ core/visual/pixel_vis_test.go
@@ -0,0 +1,135 @@
+package visual
+
+import (
+	"context"
+	"flag"
+	"testing"
+	"time"
+
+	pb "github.com/kkoralsky/gosprints/proto"
+)
+
+var (
+	vis VisInterface
+)
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	vis, _ = SetupVis("testing 1 2 3", "clock", false, 640, 480, 1)
+	go func() {
+		time.Sleep(1100 * time.Millisecond) // wait for visualization to setup
+		m.Run()
+	}()
+	vis.Run()
+}
+
+func Test_PixelVis_NewRace(t *testing.T) {
+	vis.NewTournament(context.Background(), &pb.Tournament{
+		Color: []string{"blue", "red"}, DestValue: 400, Mode: 0,
+		Name: "testing 1 2 3", PlayerCount: 2})
+	vis.NewRace(context.Background(), &pb.Race{
+		DestValue: 400,
+		Players: []*pb.Player{
+			&pb.Player{Name: "player1"},
+			&pb.Player{Name: "player2"},
+		},
+	})
+}
+
+func Test_PixelVis_ShowResults(t *testing.T) {
+	var resultSet = []pb.Results{
+		pb.Results{Result: []*pb.Result{
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 231.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 231.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 2931.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 231.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 231.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 231.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 231.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 231.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 231.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 231.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 231.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 231.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 231.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 231.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 231.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 23031.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 231.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 231.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 44231.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 1.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 231.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 29931.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 2931.3},
+			&pb.Result{DestValue: 10, Player: &pb.Player{Gender: pb.Gender(0), Name: "ktos"}, Result: 231.3},
+		},
+		},
+	}
+
+	for _, results := range resultSet {
+		vis.ShowResults(context.Background(), &results)
+		time.Sleep(2 * time.Second)
+	}
+}
+
+func Test_PixelVis_Abort(t *testing.T) {
+	vis.AbortRace(context.Background(), &pb.AbortMessage{"aborted"})
+	time.Sleep(1 * time.Second)
+	vis.AbortRace(context.Background(), &pb.AbortMessage{"falsestart"})
+	time.Sleep(1 * time.Second)
+	vis.AbortRace(context.Background(), &pb.AbortMessage{"fdospf safdso pfs fopsafa fd a"})
+}
+
+func Test_ClockVis_StartRace(t *testing.T) {
+	vis.NewTournament(context.Background(), &pb.Tournament{
+		Color: []string{"blue", "red"}, DestValue: 400, Mode: 0,
+		Name: "testing 1 2 3", PlayerCount: 2})
+	vis.NewRace(context.Background(), &pb.Race{
+		DestValue: 400,
+		Players: []*pb.Player{
+			&pb.Player{Name: "koral"},
+			&pb.Player{Name: "koral2"},
+		},
+	})
+	vis.StartRace(context.Background(), &pb.Starter{3})
+}
+
+func Test_ClockVis_UpdateRace(t *testing.T) {
+	vis.NewTournament(context.Background(), &pb.Tournament{
+		Color: []string{"blue", "red"}, DestValue: 400, Mode: 0,
+		Name: "testing 1 2 3", PlayerCount: 2})
+	vis.NewRace(context.Background(), &pb.Race{
+		DestValue: 400,
+		Players: []*pb.Player{
+			&pb.Player{Name: "player1"},
+			&pb.Player{Name: "player2"},
+		},
+	})
+	stream := NewmockRaces_UpdateRaceServer(5000, 1000, 50, 20, 2, 550)
+	for _, racer := range stream.racers {
+		t.Logf("%d: %d\n", racer.PlayerNum, racer.Distance)
+	}
+	vis.UpdateRace(stream)
+}
+
+func Benchmark_ClockVis_UpdateRace(b *testing.B) {
+	vis.NewTournament(context.Background(), &pb.Tournament{
+		Color: []string{"blue", "red"}, DestValue: 400, Mode: 0,
+		Name: "testing 1 2 3", PlayerCount: 2})
+	vis.NewRace(context.Background(), &pb.Race{
+		DestValue: 400,
+		Players: []*pb.Player{
+			&pb.Player{Name: "player1"},
+			&pb.Player{Name: "player2"},
+		},
+	})
+
+	stream := NewmockRaces_UpdateRaceServer(0, 0, 50, 10, 2, 4)
+	b.Run("4 clock moves", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			vis.UpdateRace(stream)
+			stream.currentRacer = 0
+		}
+	})
+}
---
+++ core/visual/update_mock.go
@@ -0,0 +1,70 @@
+package visual
+
+import (
+	pb "github.com/kkoralsky/gosprints/proto"
+	"io"
+	"math/rand"
+	"time"
+)
+
+type mockRaces_UpdateRaceServer struct {
+	pb.Races_UpdateRaceServer
+	racers       []*pb.Racer
+	maxWait      int
+	minWait      int
+	currentRacer int
+}
+
+func NewmockRaces_UpdateRaceServer(
+	maxWait, minWait,
+	maxDistanceDelta, minDistanceDelta,
+	playerCount, racersCount int) *mockRaces_UpdateRaceServer {
+
+	rand.Seed(600)
+	var (
+		m = &mockRaces_UpdateRaceServer{
+			minWait:      minWait,
+			maxWait:      maxWait,
+			currentRacer: 0,
+		}
+		prevDistance = 0
+		currPlayer   = 0
+	)
+
+	for i := 0; i < playerCount; i++ {
+		m.racers = append(m.racers, &pb.Racer{
+			PlayerNum: uint32(i),
+			Distance:  0,
+		})
+	}
+
+	for i := playerCount; i < racersCount; i++ {
+		prevDistance = int(m.racers[i-currPlayer-playerCount].Distance)
+
+		m.racers = append(m.racers, &pb.Racer{
+			PlayerNum: uint32(currPlayer),
+			Distance:  uint32(prevDistance + rand.Intn(maxDistanceDelta-minDistanceDelta) + minDistanceDelta),
+		})
+
+		currPlayer++
+		if currPlayer == playerCount {
+			currPlayer = 0
+		}
+	}
+	return m
+}
+
+func (m *mockRaces_UpdateRaceServer) SendAndClose(*pb.Empty) error {
+	return nil
+}
+
+func (m *mockRaces_UpdateRaceServer) Recv() (*pb.Racer, error) {
+	m.currentRacer++
+	if m.maxWait != 0 {
+		time.Sleep(time.Duration(rand.Intn(m.maxWait-m.minWait)+m.minWait) * time.Nanosecond)
+	}
+	if m.currentRacer >= len(m.racers) {
+		return nil, io.EOF
+	}
+	return m.racers[m.currentRacer], nil
+}
