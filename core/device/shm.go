3ae1eae548a1fbf872e3749884eb4d8e6dde66bd
Karol Ossowski <karol.ossowski@gmail.com>
2018-07-20 23:06:35 +0200

    encode binary from counter, stream its output (stdout,stderr)

--- core/device/shm.go
+++ core/device/shm.go
@@ -1,8 +1,11 @@
 package device
 
 import (
+	"encoding/binary"
 	"fmt"
+	log "github.com/kkoralsky/gosprints/core"
 	"github.com/pkg/errors"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -14,6 +17,7 @@ import (
 const (
 	shmResetSignal              = syscall.SIGABRT
 	shmCloseSignal              = syscall.SIGTERM
+	shmMapSize                  = 4
 	shmPrefix                   = "/gosprints"
 	shmDevice                   = "/dev/shm"
 	defaultShmCounterExecutable = "raspio/goldio"
@@ -36,7 +40,7 @@ type ShmReader struct {
 }
 
 // Init creates SHM "sockets" where input device data will be written
-func (s *ShmReader) Init(players []string, samplingRate uint, falseStart uint) error {
+func (s *ShmReader) Init(ports []string, samplingRate uint, falseStart uint) error {
 	var (
 		counterExecPath        string
 		counterArgs            []string
@@ -48,7 +52,9 @@ func (s *ShmReader) Init(players []string, samplingRate uint, falseStart uint) e
 	if !found {
 		counterExecPath = defaultShmCounterExecutable
 	}
-
+	if _, found = os.LookupEnv(shmSudoEnv); found {
+		counterArgs[0] = counterExecPath
+	}
 	if _, found = os.LookupEnv(shmPullUpEnv); found {
 		counterArgs = append(counterArgs, "-p")
 	}
@@ -59,12 +65,12 @@ func (s *ShmReader) Init(players []string, samplingRate uint, falseStart uint) e
 		counterArgs = append(counterArgs, fmt.Sprintf("-w %s", counterSleep))
 	}
 
-	counterArgs = append(counterArgs, fmt.Sprintf("-t %d", samplingRate), strings.Join(players, ","))
+	counterArgs = append(counterArgs, fmt.Sprintf("-t %d", samplingRate), strings.Join(ports, ","))
 
-	if _, found = os.LookupEnv(shmSudoEnv); found {
-		s.counterCmd = exec.Command("sudo", counterExecPath, strings.Join(counterArgs, " "))
+	if counterExecPath == counterArgs[0] {
+		s.counterCmd = exec.Command("sudo", counterArgs...)
 	} else {
-		s.counterCmd = exec.Command(counterExecPath, strings.Join(counterArgs, " "))
+		s.counterCmd = exec.Command(counterExecPath, counterArgs...)
 	}
 
 	s.counterCmd.Env = os.Environ()
@@ -74,10 +80,10 @@ func (s *ShmReader) Init(players []string, samplingRate uint, falseStart uint) e
 
 	s.threshold = samplingRate
 	s.falseStart = falseStart
-	s.playersNum = uint(len(players))
+	s.playersNum = uint(len(ports))
 
-	for i := 0; i < len(players); i++ {
-		filename := filepath.Join(shmDevice, shmPrefix+players[i])
+	for i := 0; i < len(ports); i++ {
+		filename := filepath.Join(shmDevice, fmt.Sprintf("%s%d", shmPrefix, i))
 		file, err := os.OpenFile(filename, os.O_CREATE|os.O_RDONLY, 0666)
 		file.Truncate(0)
 		if err != nil {
@@ -91,9 +97,31 @@ func (s *ShmReader) Init(players []string, samplingRate uint, falseStart uint) e
 
 // Start starts a counter program that will write distances into SHM files
 func (s *ShmReader) Start() error {
-	if err := s.counterCmd.Start(); err != nil {
+	var (
+		err                                      error
+		counterStdOutReader, counterStdErrReader io.ReadCloser
+	)
+	counterStdOutReader, err = s.counterCmd.StdoutPipe()
+	if err != nil {
 		return err
 	}
+	counterStdErrReader, err = s.counterCmd.StderrPipe()
+	if err != nil {
+		return err
+	}
+
+	if err = s.counterCmd.Start(); err != nil {
+		return err
+	}
+	counterReader := io.MultiReader(counterStdErrReader, counterStdOutReader)
+
+	go func() {
+		_, err := io.Copy(os.Stdout, counterReader)
+		if err != nil {
+			log.ErrorLogger.Printf("error on copying counter output: %v", err)
+		}
+	}()
+
 	s.counterProcess = s.counterCmd.Process
 	return nil
 }
@@ -101,20 +129,22 @@ func (s *ShmReader) Start() error {
 // GetDist reads current distance of a player from a SHM file
 func (s *ShmReader) GetDist(playerID uint) (uint, error) {
 	var (
-		b      []byte
+		b      = make([]byte, shmMapSize)
 		res    uint64
-		errCtx = fmt.Sprintf("SHM read for player %d failed", playerID)
+		n      int
+		err    error
+		errCtx = fmt.Sprintf("read from %s failed", s.files[playerID].Name())
 	)
+	s.files[playerID].Seek(0, 0)
 	if _, err := s.files[playerID].Read(b); err != nil {
 		return 0, errors.Wrap(err, errCtx)
 	}
 
-	res, err := strconv.ParseUint(string(b), 10, 64)
+	res, n = binary.Uvarint(b)
 
-	if err != nil {
+	if n < 0 {
 		return 0, errors.Wrap(err, errCtx)
 	}
-	s.files[playerID].Seek(0, 0)
 	return uint(res), nil
 }
 
