35a7824b8d02bd697b15646a72bcf8f2ef8de91c
Karol Ossowski <karol.ossowski@gmail.com>
2018-07-22 22:44:52 +0200

    racing logic

--- core/server/sprints.go
+++ core/server/sprints.go
@@ -2,8 +2,9 @@ package server
 
 import (
 	"context"
+	"errors"
 	"fmt"
-	log "github.com/kkoralsky/gosprints/core"
+	"github.com/kkoralsky/gosprints/core"
 	"github.com/kkoralsky/gosprints/core/device"
 	pb "github.com/kkoralsky/gosprints/proto"
 	"time"
@@ -15,17 +16,20 @@ type Sprints struct {
 	tournament  *pb.Tournament
 	curRace     *pb.Race
 	results     map[int32][]*pb.Result
+	abortRace   chan struct{}
 }
 
 func SetupSprints(device device.InputDevice, visMux *VisMux) *Sprints {
 	return &Sprints{
 		inputDevice: device,
 		visMux:      visMux,
+		tournament:  &core.DefultTournament,
 	}
 }
 
 func (s *Sprints) NewTournament(ctx context.Context, tournament *pb.Tournament) (*pb.Tournament, error) {
 	s.tournament = tournament
+	core.DebugLogger.Printf("%s", pb.Tournament_TournamentMode_name[int32(s.tournament.Mode)])
 	return tournament, s.visMux.NewTournament(tournament)
 }
 
@@ -36,8 +40,12 @@ func (s *Sprints) NewRace(ctx context.Context, race *pb.Race) (*pb.Empty, error)
 }
 
 func (s *Sprints) StartRace(_ context.Context, starter *pb.Starter) (*pb.Player, error) {
+	if s.curRace == nil {
+		return &pb.Player{}, errors.New("race is not established")
+	}
 	s.visMux.StartRace(starter)
 	s.inputDevice.Clean()
+	s.abortRace = make(chan struct{}, 10) // allow to up to 10 unhandled "aborts"
 	time.Sleep(time.Duration(starter.CountdownTime) * time.Second)
 	if playerNum, err := s.inputDevice.Check(); err != nil {
 		return &pb.Player{}, err
@@ -54,7 +62,10 @@ func (s *Sprints) StartRace(_ context.Context, starter *pb.Starter) (*pb.Player,
 }
 
 func (s *Sprints) AbortRace(_ context.Context, abortMessage *pb.AbortMessage) (*pb.Empty, error) {
-	s.visMux.AbortRace(abortMessage)
+	if s.curRace != nil {
+		s.abortRace <- struct{}{}
+		s.visMux.AbortRace(abortMessage)
+	}
 
 	return &pb.Empty{}, nil
 }
@@ -79,40 +90,94 @@ func (s *Sprints) GetTournaments(*pb.Empty, pb.Sprints_GetTournamentsServer) err
 }
 
 func (s *Sprints) doRace() {
-}
-
-func (s *Sprints) doDistanceRace() {
-}
-
-func (s *Sprints) doTimedRace() {
 	var (
-		start        = time.Now()
-		raceDuration = time.Duration(s.curRace.DestValue) * time.Second
-		finish       = start.Add(raceDuration)
-		playerCount  = len(s.curRace.Players)
-		dists        []uint
+		playersCount = len(s.curRace.Players)
+		playersDists = make(map[int]uint, playersCount)
+		results      map[int]uint
+		getDistance  = func(i int) uint {
+			if dist, err := s.inputDevice.GetDist(uint(i)); err != nil {
+				core.DebugLogger.Printf(err.Error())
+			} else if dist != playersDists[i] {
+				playersDists[i] = dist
+				s.visMux.SendRaceUpdate(uint32(i), uint32(dist))
+			}
+			return playersDists[i]
+		}
+		doDistanceRace = func() (playersTimes map[int]uint) {
+			var (
+				wholeDistance = uint(s.curRace.DestValue)
+				start         = time.Now()
+			)
+			playersTimes = make(map[int]uint, playersCount)
+
+			for playersFinished := 0; playersFinished < playersCount; {
+				for i := 0; i < playersCount; i++ {
+					select {
+					case _, ok := <-s.abortRace:
+						if ok {
+							close(s.abortRace)
+						}
+						return
+					default:
+						if getDistance(i) >= wholeDistance {
+							if playersTimes[i] == 0 {
+								playersFinished++
+								playersTimes[i] = uint(time.Now().Sub(start))
+								core.DebugLogger.Printf("player #%d finished", i)
+							}
+						}
+					}
+				}
+				time.Sleep(50 * time.Millisecond)
+			}
+			return
+		}
+		doTimedRace = func() map[int]uint {
+			var (
+				start        = time.Now()
+				raceDuration = time.Duration(s.curRace.DestValue) * time.Second
+				finish       = start.Add(raceDuration)
+			)
+
+			for now := time.Now(); now.Before(finish); now = time.Now() {
+				for i := 0; i < playersCount; i++ {
+					select {
+					case <-s.abortRace:
+						close(s.abortRace)
+						return playersDists
+					default:
+						getDistance(i)
+					}
+				}
+				time.Sleep(50 * time.Millisecond)
+			}
+			return playersDists
+		}
+		finishRace = func(results map[int]uint) {
+			var protoResults []*pb.Result
+
+			for playerNum, result := range results {
+				protoResults = append(protoResults, &pb.Result{
+					DestValue: s.curRace.DestValue,
+					Player:    s.curRace.Players[playerNum],
+					Result:    float32(result),
+				})
+			}
+
+			s.curRace = nil
+			s.visMux.FinishRace(&pb.Results{Result: protoResults})
+		}
 	)
 
 	s.visMux.SetupRacers()
 
-	for i := 0; i < playerCount; i++ {
-		dists[i] = 0
-	}
-
-	for now := time.Now(); now.Before(finish); now = time.Now() {
-		for i := 0; i < len(s.curRace.Players); i++ {
-			dist, err := s.inputDevice.GetDist(uint(i))
-			if err != nil {
-				log.DebugLogger.Printf(err.Error())
-				continue
-			}
-			if dist != dists[i] {
-				dists[i] = dist
-				s.visMux.SendRaceUpdate(uint32(i), uint32(dist))
-			}
-		}
-		time.Sleep(50 * time.Millisecond)
+	core.DebugLogger.Printf("%s", pb.Tournament_TournamentMode_name[int32(s.tournament.Mode)])
+	if s.tournament.Mode == pb.Tournament_TIME {
+		results = doTimedRace()
+	} else if s.tournament.Mode == pb.Tournament_DISTANCE {
+		results = doDistanceRace()
 	}
 
 	s.visMux.CloseRacers()
+	finishRace(results)
 }
